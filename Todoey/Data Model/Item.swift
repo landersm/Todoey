75baca2c73cf30b0e6f6058ed92c6a032d0b07c8
Miguel Lander <miguellanders@yahoo.com>
2018-11-09 15:48:54 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Miguel Landers on 2018-11-08.
+//  Copyright © 2018 FpertSoft. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
